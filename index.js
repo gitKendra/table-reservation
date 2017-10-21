77c84c5e7a512cf3ebab5ae2f90fea08ac052749
gitKendra <kendra.krzywicki@knights.ucf.edu>
2017-10-21 11:31:08 -0400

    working app

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require("path");
+var tables = require('./api/tables.json');
+var waitlist = require('./api/waitlist.json');
 
 var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-var reservations = [];
-var waitlist = [];
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "home.html"));
@@ -19,12 +19,27 @@ app.get("/reservation", function(req, res) {
   res.sendFile(path.join(__dirname, "reservation.html"));
 });
 
-app.get("/table", function(req, res) {
+app.get("/tables", function(req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
 });
 
-app.post("/api/table", function(req, res) {
-  res.json(tables);
+app.get("/api/tables", function(req, res) {
+	res.json(tables);
+});
+
+app.get("/api/waitlist", function(req, res) {
+	res.json(waitlist);
+});
+
+app.post("/api/tables", function(req, res){
+	var newTable = req.body;
+	if(tables.length < 5){
+		tables.push(newTable);
+	}
+	else{
+		waitlist.push(newTable);
+	}
+	res.json(newTable);
 });
 
 app.listen(PORT,function(){
