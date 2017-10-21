5f6c5d44c80ee4aa0ea09154452cb7380f07cc33
gitKendra <kendra.krzywicki@knights.ucf.edu>
2017-10-21 09:53:35 -0400

    initial commit correction3

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-var app = require('express')();
-var bodyParser = require('body-parser');
-
+var express = require('express');
+var app = express();
+var bodyParser = require("body-parser");
 var PORT = process.env.PORT || 3000;
-
-// boilerplate for app.use
+// Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.get('/', function(req, res){
-	res.send("Some text");
+app.get("/", function(req, res) {
+  res.send("some thing very cool");
 });
-
-
-app.listen(PORT, function(){
-	console.log("listening on port " + PORT);
+app.listen(PORT,function(){
+    console.log("listening to port %s",PORT);
 });
